7bc2a80709468a78afe0796737818dbb6364ddb0
MarkusWb <wima771@gmx.de>
2017-02-01 00:06:50 +0100

    added base64 decoding

--- src/main/java/de/axxepta/converterservices/App.java
+++ src/main/java/de/axxepta/converterservices/App.java
@@ -1,5 +1,10 @@
 package de.axxepta.converterservices;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemHeaders;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.impl.SimpleLogger;
@@ -14,9 +19,7 @@ import javax.servlet.http.Part;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 import static spark.Spark.*;
 
@@ -24,16 +27,38 @@ public class App {
 
     private static Logger logger;
 
-    private static String TEMP_FILE_PATH = System.getProperty("user.home") + "/.converterservices";
+    private static final String STATIC_FILE_PATH = System.getProperty("user.home") + "/.converterservices";
+    private static final String TEMP_FILE_PATH = STATIC_FILE_PATH + "/temp";
+
+    private static final String PATH_HELLO              = "/hello";
+    private static final String PATH_STOP               = "/stop";
+    private static final String PATH_THUMB              = "/file/thumb";
+    private static final String PATH_THUMBS             = "/file/thumbs";
+    private static final String PATH_META               = "/file/meta";
+    private static final String PATH_META64             = "/file/meta64";
+    private static final String PATH_SPLIT              = "/pdfsplit";
+    private static final String PATH_DOWNLOAD           = "/static";
+    private static final String PATH_UPLOAD             = "/files/upload";
+    private static final String PATH_UPLOAD_THUMB       = "/file/thumbupload";
+    private static final String PATH_UPLOAD_THUMBS      = "/files/thumbupload";
+    private static final String PATH_UPLOAD_META        = "/file/metaupload";
+    private static final String PATH_UPLOAD_SPLIT       = "/file/pdfsplitupload";
+
+    private static final String PARAM_NAME              = ":name";
 
     private static final String THUMB_UPLOAD_FORM       = "static/form_thumb.html";
     private static final String THUMBS_UPLOAD_FORM      = "static/form_thumbs.html";
     private static final String META_UPLOAD_FORM        = "static/form_meta.html";
+    private static final String PDFSPLIT_UPLOAD_FORM    = "static/form_pdfsplit.html";
 
     private static final String TYPE_JPEG               = "image/jpeg";
+    private static final String TYPE_PDF                = "application/pdf";
 
     private static final String FILE_PART               = "FILE";
+    private static final String TO_IMAGE                = "IS_IMAGE";
+    private static final String MULTIPART_FORM_DATA     = "multipart/form-data";
     private static final String MULTI_PART_BOUNDARY     = "MULTI_PART_BOUNDARY";
+    private static final String CONTENT_TRANSFER_ENCODING     = "Content-Transfer-Encoding";
     private static final String HTML_OPEN               = "<html><body><h1>";
     private static final String HTML_CLOSE              = "</h1></body></html>";
     private static final String NO_SUCH_FILE            = "<html><body><h1>File does not exist.</h1></body></html>";
@@ -47,14 +72,14 @@ public class App {
         System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "ERROR");
         logger = LoggerFactory.getLogger(App.class);
         try {
-            safeCreateDirectory(TEMP_FILE_PATH);
+            de.axxepta.converterservices.IOUtils.safeCreateDirectory(TEMP_FILE_PATH);
         } catch (IOException ie) {
             logger.error("Couldn't create directory for temporary files!");
         }
 
-        get("/hello", (request, response) -> "hello");
+        get(PATH_HELLO, (request, response) -> "hello");
 
-        post("/file/thumb", "multipart/form-data", (request, response) -> {
+        post(PATH_THUMB, MULTIPART_FORM_DATA, (request, response) -> {
             List<String> files;
             try {
                 files = thumbifyFiles(request);
@@ -63,12 +88,12 @@ public class App {
             }
 
             if (files.size() > 0) {
-                HttpServletResponse raw = singleFileResponse(response, request.params("thumb_" + jpgFilename(files.get(0))));
+                HttpServletResponse raw = singleFileResponse(response, "thumb_" + jpgFilename(files.get(0)));
                 File file = new File(TEMP_FILE_PATH + "/" + jpgFilename(files.get(0)));
                 if (file.exists()) {
                     files.add(jpgFilename(files.get(0)));
                     try (InputStream is = new FileInputStream(file)) {
-                        copyStreams(is, raw.getOutputStream());
+                        de.axxepta.converterservices.IOUtils.copyStreams(is, raw.getOutputStream());
                         raw.getOutputStream().close();
                         return raw;
                     } catch (Exception e) {
@@ -85,7 +110,7 @@ public class App {
             }
         });
 
-        post("/file/thumbs", "multipart/form-data", (request, response) -> {
+        post(PATH_THUMBS, MULTIPART_FORM_DATA, (request, response) -> {
             List<String> files;
             try {
                 files = thumbifyFiles(request);
@@ -119,12 +144,12 @@ public class App {
             }
         });
 
-        get("/static/:name", (request, response) -> {
-            HttpServletResponse raw = singleFileResponse(response, request.params(":name"));
-            File file = new File(TEMP_FILE_PATH + "/" + request.params(":name"));
+        get(PATH_DOWNLOAD + "/" + PARAM_NAME, (request, response) -> {
+            HttpServletResponse raw = singleFileResponse(response, request.params(PARAM_NAME));
+            File file = new File(STATIC_FILE_PATH + "/" + request.params(PARAM_NAME));
             if (file.exists()) {
                 try (InputStream is = new FileInputStream(file)) {
-                    copyStreams(is, raw.getOutputStream());
+                    de.axxepta.converterservices.IOUtils.copyStreams(is, raw.getOutputStream());
                     raw.getOutputStream().close();
                 } catch (Exception e) {
                     return HTML_OPEN + e.getMessage() + HTML_CLOSE;
@@ -135,20 +160,24 @@ public class App {
             return raw;
         });
 
-        get("/file/thumbupload", (request, response) ->
-                String.format(getResource(THUMB_UPLOAD_FORM), FILE_PART)
+        get(PATH_UPLOAD_THUMB, (request, response) ->
+                String.format(de.axxepta.converterservices.IOUtils.getResource(THUMB_UPLOAD_FORM), PATH_THUMB, FILE_PART)
         );
 
-        get("/files/thumbupload", (request, response) ->
-                String.format(getResource(THUMBS_UPLOAD_FORM), FILE_PART)
+        get(PATH_UPLOAD_THUMBS, (request, response) ->
+                String.format(de.axxepta.converterservices.IOUtils.getResource(THUMBS_UPLOAD_FORM), PATH_THUMBS, FILE_PART)
         );
 
-        get("/file/metaupload", (request, response) ->
-                String.format(getResource(META_UPLOAD_FORM), FILE_PART)
+        get(PATH_UPLOAD_META, (request, response) ->
+                String.format(de.axxepta.converterservices.IOUtils.getResource(META_UPLOAD_FORM), PATH_META, FILE_PART)
         );
 
-        post("/files/upload", "multipart/form-data", (request, response) -> {
-            MultipartConfigElement multipartConfigElement = new MultipartConfigElement(TEMP_FILE_PATH);
+        get(PATH_UPLOAD_SPLIT, (request, response) ->
+                String.format(de.axxepta.converterservices.IOUtils.getResource(PDFSPLIT_UPLOAD_FORM), PATH_SPLIT, FILE_PART, TO_IMAGE)
+        );
+
+        post(PATH_UPLOAD, MULTIPART_FORM_DATA, (request, response) -> {
+            MultipartConfigElement multipartConfigElement = new MultipartConfigElement(STATIC_FILE_PATH);
             request.raw().setAttribute("org.eclipse.jetty.multipartConfig", multipartConfigElement);
 
             Collection<Part> parts = null;
@@ -169,8 +198,8 @@ public class App {
             return builder.append("</body></html>").toString();
         });
 
-        post("/file/meta", (request, response) -> {
-            List<String > files = storeFilesTemporary(request);
+        post(PATH_META, (request, response) -> {
+            List<String> files = storeFilesTemporary(request);
             try {
                 if (files.size() > 0) {
                     ByteArrayOutputStream out =
@@ -179,17 +208,69 @@ public class App {
                     return new String(out.toByteArray(), "UTF-8");
                 } else return NO_FILES_JSON;
             } catch (Exception e) {
+                e.printStackTrace();
                 return EXCEPTION_OPEN_JSON + e.getMessage() + EXCEPTION_CLOSE_JSON;
             }
         });
 
-        get("/stop", (request, response) -> {
+        post(PATH_META64, (request, response) -> {
+            List<String> files = storeFilesTemporary(request);
+            try {
+                if (files.size() > 0) {
+                    ByteArrayOutputStream out =
+                            runExternal("exiftool", "-e", "-json", STATIC_FILE_PATH + "/" + files.get(0));
+                    cleanTemp(files);
+                    return new String(out.toByteArray(), "UTF-8");
+                } else return NO_FILES_JSON;
+            } catch (Exception e) {
+                return EXCEPTION_OPEN_JSON + e.getMessage() + EXCEPTION_CLOSE_JSON;
+            }
+        });
+
+        post(PATH_SPLIT, MULTIPART_FORM_DATA, (request, response) -> {
+            List<String> files;
+            List<String> outputFiles;
+            boolean toImage;
+            try {
+                files = storeFilesTemporary(request);
+                toImage = readImagePart(request);
+                outputFiles = PDFUtils.splitPDF(files.get(0), toImage, TEMP_FILE_PATH);
+            } catch (IOException | InterruptedException ex) {
+                return HTML_OPEN + ex.getMessage() + HTML_CLOSE;
+            }
+
+            if (outputFiles.size() > 0) {
+                try {
+                    HttpServletResponse raw = multiPartResponse(response);
+                    for (String fileName : outputFiles) {
+                        File file = new File(TEMP_FILE_PATH + "/" + fileName);
+                        if (file.exists()) {
+                            files.add(fileName);
+                            try (InputStream is = new BufferedInputStream(new FileInputStream(file))) {
+                                addMultiPartFile(raw.getOutputStream(), TYPE_PDF, is, fileName);
+                            }
+                        }
+                    }
+                    multiPartClose(raw.getOutputStream());
+                    return raw;
+                } catch (IOException ex) {
+                    return HTML_OPEN + ex.getMessage() + HTML_CLOSE;
+                } finally {
+                    cleanTemp(files);
+                }
+            } else {
+                return NO_SUCH_FILE;
+            }
+        });
+
+        get(PATH_STOP, (request, response) -> {
             stop();
             return "Services stopped.";
         });
 
     }
 
+//########################################################
 
     private static List<String> thumbifyFiles(Request request) throws IOException, InterruptedException {
         List<String> files = storeFilesTemporary(request);
@@ -226,7 +307,7 @@ public class App {
         os.println("Content-Disposition: attachment; filename=\"" + name + "\"");
         os.println("Content-Type: " + contentType);
         os.println();
-        copyStreams(is, os);
+        de.axxepta.converterservices.IOUtils.copyStreams(is, os);
         os.println("--" + MULTI_PART_BOUNDARY);     // ?
         os.flush();
     }
@@ -243,32 +324,59 @@ public class App {
         return name.substring(0, (pos == -1) ? name.length() : pos) + ".jpg";
     }
 
-    private static void copyStreams(InputStream is, OutputStream os) throws IOException {
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = is.read(buffer, 0, buffer.length)) > -1) {
-            os.write(buffer, 0, length);
-            os.flush();
-        }
-    }
-
-    private static List<String> storeFilesTemporary(Request request) {
-        MultipartConfigElement multipartConfigElement = new MultipartConfigElement(TEMP_FILE_PATH);
+    private static boolean readImagePart(Request request) {
+        MultipartConfigElement multipartConfigElement = new MultipartConfigElement(STATIC_FILE_PATH);
         request.raw().setAttribute("org.eclipse.jetty.multipartConfig", multipartConfigElement);
         Collection<Part> parts;
-        List<String> files = new ArrayList<>();
         try {
             parts = request.raw().getParts();
             if (parts != null) {
                 for (Part part : parts) {
-                    if (part.getName().equals(FILE_PART)) {
-                        part.write(part.getSubmittedFileName());
-                        files.add(part.getSubmittedFileName());
+                    if (part.getName().equals(TO_IMAGE)) {
+                        return true;
                     }
                 }
             }
         } catch (IOException | ServletException ex) {
             if (logger != null) logger.error(ex.getMessage());
+        }
+        return false;
+    }
+
+    private static List<String> storeFilesTemporary(Request request) {
+        MultipartConfigElement multipartConfigElement = new MultipartConfigElement(TEMP_FILE_PATH);
+        request.raw().setAttribute("org.eclipse.jetty.multipartConfig", multipartConfigElement);
+
+        List<String> files = new ArrayList<>();
+        File upload = new File(TEMP_FILE_PATH);
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        factory.setRepository(upload);
+        ServletFileUpload fileUpload = new ServletFileUpload(factory);
+        try {
+            List<FileItem> items = fileUpload.parseRequest(request.raw());
+            items.stream()
+                    .filter(e -> FILE_PART.equals(e.getFieldName()))
+                    .forEach(item -> {
+                        try {
+                            String fileName = item.getName();
+                            FileItemHeaders headers = item.getHeaders();
+                            if ((headers.getHeader(CONTENT_TRANSFER_ENCODING) != null)
+                                    && headers.getHeader(CONTENT_TRANSFER_ENCODING).toLowerCase().equals("base64")) {
+                                InputStream is = item.getInputStream();
+                                byte[] bytesFromBase64 = org.apache.commons.codec.binary.Base64.decodeBase64(IOUtils.toByteArray(is));
+                                try (FileOutputStream os = new FileOutputStream(TEMP_FILE_PATH + "/" + fileName)) {
+                                    os.write(bytesFromBase64);
+                                }
+                            } else {
+                                item.write(new File(TEMP_FILE_PATH, fileName));
+                            }
+                            files.add(fileName);
+                        } catch (Exception ex) {
+                            if (logger != null) logger.error(ex.getMessage());
+                        }
+            });
+        } catch (Exception fu) {
+            if (logger != null) logger.error(fu.getMessage());
         }
         return files;
     }
@@ -294,8 +402,8 @@ public class App {
                     stream.write(buffer, 0, n);
                 }
             }
-            String line;
             try (BufferedReader bre = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
+                String line;
                 while ((line = bre.readLine()) != null) {
                     if (logger != null) logger.error(line);
                 }
@@ -307,19 +415,6 @@ public class App {
             throw err;
         }
         return stream;
-    }
-
-    private static void safeCreateDirectory(String path) throws IOException {
-        if (!Files.exists(Paths.get(path)))
-            Files.createDirectory(Paths.get(path));
-    }
-
-    private static String getResource(String name) throws IOException {
-        final String resource = "/" + name;
-        final InputStream is = App.class.getResourceAsStream(resource);
-        if(is == null) throw new IOException("Resource not found: " + resource);
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
     }
 
 }
---
+++ src/main/java/de/axxepta/converterservices/PDFUtils.java
@@ -0,0 +1,73 @@
+package de.axxepta.converterservices;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.tools.imageio.ImageIOUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PDFUtils {
+
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
+    private PDFUtils() {}
+
+    static List<String> splitPDF(String sourcePath, String targetPath, boolean imageOutput, List<Integer> widthList, List<Integer> heightList) {
+        List<String> pdfOutputList = new ArrayList<>();
+        Integer pageNumber = 0;
+        try {
+            File pdfFile = new File(sourcePath);
+            if (pdfFile.exists()) {
+                String fileName = targetPath.replace(".pdf", "");
+                String pathName;
+                String targetDir = IOUtils.dirFromPath(targetPath);
+                IOUtils.safeCreateDirectory(targetDir);
+                PDDocument document = PDDocument.load(pdfFile);
+                PDFRenderer pdfRenderer = new PDFRenderer(document);
+                PDPageTree pages = document.getPages();
+                for (PDPage page : pages) {
+                    pageNumber++;
+                    System.out.println(pageNumber - 1);
+                    if (imageOutput) {
+                        BufferedImage bim = pdfRenderer.renderImageWithDPI(pageNumber - 1, 300, ImageType.RGB);
+                        pathName = fileName + "_" + pageNumber + ".png";
+                        widthList.add(bim.getWidth());
+                        heightList.add(bim.getHeight());
+                        ImageIOUtil.writeImage(bim, pathName, 300);
+                    } else {
+                        PDDocument newDocument = new PDDocument();
+                        newDocument.addPage(page);
+                        pathName = fileName + "_" + pageNumber + ".pdf";
+                        File newFile = new File(pathName);
+                        if (!newFile.createNewFile()) {
+                            logger.error("Exception splitting PDF resource: File " + pathName + " could not be created.");
+                        }
+                        newDocument.save(newFile);
+                        newDocument.close();
+                    }
+                    pdfOutputList.add(pathName);
+                }
+            } else {
+                logger.error("Input file for splitting not found: " + sourcePath);
+            }
+        } catch (IOException ex) {
+            logger.error("Exception splitting PDF resource: " + ex.getMessage());
+        }
+        return pdfOutputList;
+    }
+
+    static List<String> splitPDF(String inputFile, boolean convertToPNG, String path) throws IOException, InterruptedException {
+        List<Integer> wList = new ArrayList<>();
+        List<Integer> hList = new ArrayList<>();
+        return splitPDF(path + "/" + inputFile, path + "/" + inputFile, convertToPNG, wList, hList);
+    }
+}

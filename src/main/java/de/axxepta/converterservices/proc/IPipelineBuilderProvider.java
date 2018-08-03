c7a9c278e826ebb5a0eb4641bd00c5d707d179a7
MarkusWb <wima771@gmx.de>
2018-08-03 14:21:27 +0200

    v0.3.0

--- src/main/java/de/axxepta/converterservices/proc/IPipelineBuilderProvider.java
+++ src/main/java/de/axxepta/converterservices/proc/IPipelineBuilderProvider.java
@@ -1,6 +1,11 @@
 package de.axxepta.converterservices.proc;
 
+/**
+ * This interface has to be implemented by classes which are referenced in a pipeline XML description by the <b>class</b>
+ *     attribute in the <b>pipeline</b> element. In the <i>builder</i> function the defining builder can be prepared and
+ *     returned.
+ */
 public interface IPipelineBuilderProvider {
 
-    public Pipeline.PipelineBuilder builder();
+    Pipeline.PipelineBuilder builder();
 }
